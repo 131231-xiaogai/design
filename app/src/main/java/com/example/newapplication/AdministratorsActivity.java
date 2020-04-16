f03aa96f18aa116dd19d5e91c6aa6e7d590abd8d
131231-xiaogai <yyh1631608142057@163.com>
2020-04-16 15:58:19 +0800

    4.15-2

--- app/src/main/java/com/example/newapplication/AdministratorsActivity.java
+++ app/src/main/java/com/example/newapplication/AdministratorsActivity.java
@@ -1,13 +1,33 @@
 package com.example.newapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class AdministratorsActivity extends AppCompatActivity {
+public class AdministratorsActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView Ad_user;
 
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.administrators);}
+        setContentView(R.layout.administrators);
+        Ad_user=findViewById(R.id.Ad_user);
+
+
+        //
+        OnClickListener();
+
+    }
+
+    private void OnClickListener() {
+        Ad_user.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
 }
